8afe91c3e5e7c4ddc49a68dfd0b0484fa269c73d
science-odysseia <ysw414153@gmail.com>
2026-05-16 19:14:47 +0900

    ros

---
+++ src/my_cam_pubsub/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_cam_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubunut2204',
+    maintainer_email='ros@email.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cam_pub = my_cam_pubsub.cam_pub:main',
+            'cam_sub = my_cam_pubsub.cam_sub:main',
+        ],
+    },
+)
